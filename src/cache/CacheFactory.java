100b61b9866085ad37f5be36d77edbe6c7832d4e
Timoté Vaucher <timote.vaucher@epfl.ch>
2016-11-18 16:15:55 +0800

    Add cache controller factory

---
+++ src/cache/CacheFactory.java
@@ -0,0 +1,22 @@
+package cache;
+
+public class CacheFactory {
+	private CacheFactory() {
+
+	}
+
+	public static CacheController createCacheController(int id, String protocol, String file, int cacheSize,
+			int associativity, int blockSize) throws Exception {
+		switch (protocol) {
+		case "MSI":
+			return new MSI(id, file, cacheSize, associativity, blockSize);
+		case "MESI":
+			return new MESI(id, file, cacheSize, associativity, blockSize);
+		case "Dragon":
+		case "dragon":
+			return new Dragon(id, file, cacheSize, associativity, blockSize);
+		default:
+			return null;
+		}
+	}
+}
